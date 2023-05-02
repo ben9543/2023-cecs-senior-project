a24cee9e6f45b59c61b18ea108e8f6b26029f870
Amanuel Reda <92605110+amanuelR@users.noreply.github.com>
2023-05-01 19:34:06 -0700

    adjusting framework of the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './log-in/log-in.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HistoryComponent } from './history/history.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HelpsComponent } from './helps/helps.component';
+import { SubmitRequestComponent } from './submit-request/submit-request.component';
+import { SignupComponent } from './signup/signup.component';
+import { StudyspotViewComponent } from './studyspot-view/studyspot-view.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LogInComponent },
+  { path: 'home', component: HomepageComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'history', component: HistoryComponent},
+  { path: 'profile', component: ProfileComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'reviews', component: ReviewsComponent},
+  { path: 'help', component: HelpsComponent},
+  { path: 'submitrequest', component: SubmitRequestComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: 'studyspot-view', component: StudyspotViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
