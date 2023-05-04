77299685aa8bd10b1d9fc40bf67d395c7450754d
Amanuel Reda <92605110+amanuelR@users.noreply.github.com>
2023-05-04 09:45:43 -0700

    SS-7/SS-8/SS-39 Completed the implementation of the common header

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  
 }
