b823bed5d1a70e3b0c86d850f71f082c6a97fad6
salvador-villanueva <71253158+salvador-villanueva@users.noreply.github.com>
2023-05-07 21:19:11 -0700

    SS-7/SS-8/SS-29 Created Sign-Up Form

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-
+  signup!: FormGroup;
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
+  ngOnInit(): void {
+    this.signup = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email, Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$")]],
+      college: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+  onSubmit(): void {
+    this.router.navigate(['/login']);
+  }
 }
