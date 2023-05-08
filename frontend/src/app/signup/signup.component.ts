4dde7fbafe96a0ab3530078f75b9bb141c6f9880
the123tg30 <tony.guirguis@student.csulb.edu>
2023-05-08 14:39:46 -0700

    SS-7/SS-8/SS-29 Created Sign-Up Form

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -8,7 +10,10 @@ import { Router } from '@angular/router';
 })
 export class SignupComponent {
   signup!: FormGroup;
-  constructor(private router: Router, private formBuilder: FormBuilder) { }
+  errorMessage: string = '';
+  
+  constructor(private router: Router, private formBuilder: FormBuilder, private http: HttpClient) { }
+  
   ngOnInit(): void {
     this.signup = this.formBuilder.group({
       username: ['', Validators.required],
@@ -18,7 +23,23 @@ export class SignupComponent {
       confirmPassword: ['', Validators.required],
     });
   }
+  
   onSubmit(): void {
     this.router.navigate(['/login']);
+    const { username, college, email, password } =this.signup.value;
+    this.http.post('http://127.0.0.1:5000/api/signup', { username, college, email, password })
+    .subscribe(
+      (response) => {
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        if (error.status === 409) {
+          this.errorMessage = 'User Already Exists';
+        } else {
+          this.errorMessage = 'An unknown error occurred';
+        }
+      }
+    );
+
   }
 }
