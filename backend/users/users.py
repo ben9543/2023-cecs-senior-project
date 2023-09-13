92266334e81f610b354ebb7ff48fbd39d560b8d4
Deepmeghani <deepmeghani1@gmail.com>
2023-09-12 23:17:07 -0700

    Update API for User Class

--- backend/users/users.py
+++ backend/users/users.py
@@ -1,62 +1,70 @@
-# Create a list of users for demonstration purposes
-users = [
-    {'id': 1, 'username': 'aaa', 'email': 'a@gmail.com', 'college': 'CSULB', 'password': 'password1'},
-    {'id': 2, 'username': 'bbb', 'email': 'b@gmail.com', 'college': 'CSULB', 'password': 'password2'},
-    {'id': 3, 'username': 'ccc', 'email': 'c@gmail.com', 'college': 'CSULB', 'password': 'password3'}
-]
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-class Users:
-    def __init__(self, db):
-        self.db = db
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URI'
+db = SQLAlchemy(app)
 
-    def get_users(self):
-        return users
+# Definfing a SQLAlcehmy model for the User
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    name = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    university_id = db.Column(db.Integer)
 
-    # GET user by ID
-    def get_user(self, user_id):
-        user = next((user for user in users if user['id'] == user_id), None)
-        if user:
-            return user
-        else:
-            return None
+# API for CRUD Operations
 
-    def find_user_by_email(self, email):
-        user = next((user for user in users if user['email'] == email), None)
-        if user: 
-            return user
-        else: 
-            return None
-        
-    def add_user(self, user):
-        users.append(user)
-    
-    def count_users(self):
-        return len(users)
+# Get all users
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    users= User.query.all()
+    users_data = [{'id':user.id, 'email': user.email, 'name': user.name, 'university_id': user.univerity_id} for user in users]
+    return jsonify(users_data)
 
-    # # CREATE a user
-    # def create_user(email, password):
-    #     if email and password:
-    #         new_user = {'id': len(users) + 1, 'email': email, 'password': password}
-    #         users.append(new_user)
-    #         return (f"{new_user} had been created successfully")
-    #     else:
-    #         return "User not Found"
+# Get user by id
+@app.route('/api/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get(id)
+    if user:
+        return jsonify({'id':user.id, 'email': user.email, 'name': user.name, 'university_id': user.univerity_id})
+    else:
+        return jsonify({'message': 'User not found'}), 404
 
-    # # EDIT user profile by ID
-    # def edit_user(user_id):
-    #     user = next((user for user in users if user['id'] == user_id), None)
-    #     if user:
-    #         for key, value in request.json.items():
-    #             if key != 'email':
-    #                 user[key] = value
-    #         return (f"{user} had been created successfully")
-    #     else:
-    #         return "User not Found"
+# Create a new user
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    data = request.json
+    user = User(email=data['email'], name=data['name'], password=data['password'], univerity_id=data['university_id'])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully'}), 201
 
-    # def delete_user(user_id):
-    #     user = next((user for user in users if user['id'] == user_id), None)
-    #     if user:
-    #         users.remove(user)
-    #         (f"User had been deleted successfully")
-    #     else:
-    #         return "User not Found"
+# Update user information
+@app.route('/api/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    user = User.query.get(id)
+    if user:
+        data = request.json
+        user.name = data['name']
+        user.emial = data['email']
+        user.password = data['password']
+        user.university_id = data['university_id']
+        db.session.commit()
+        return jsonify({'message': 'User updated successfully'})
+    else:
+        return jsonify({'message': 'User not found'}), 404
+
+# Delete user from database
+@app.route('/api/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': 'User deleted successfully'})
+    else:
+        return jsonify({'message': 'User not found'}), 404
+
+if __name__ == '__main__':
+    app.run()
