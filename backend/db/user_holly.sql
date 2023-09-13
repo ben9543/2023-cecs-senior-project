849005ec9830a5aec3a5072af44a702f73039144
hollyl966 <holly.lee0001@gmail.com>
2023-09-12 23:05:20 -0700

    Update user_holly.sql

--- backend/db/user_holly.sql
+++ backend/db/user_holly.sql
@@ -21,12 +21,25 @@ CREATE TABLE Universities (
     university_zip INT NOT NULL
 );
 
+CREATE TABLE Studyspots (
+    studyspot_id INT NOT NULL,
+    PRIMARY KEY (studyspot_id),
+    university_id INT NOT NULL,
+    FOREIGN KEY (university_id) REFERENCES Universities(university_id),
+    studyspot_name VARCHAR(200),
+    /*studyspot_indoor BOOLEAN, /*indoor: TRUE, outdoor: FALSE*/*/
+    studyspot_photo VARCHAR(250),
+    studyspot_address VARCHAR(250)
+
+);
+
 CREATE TABLE Surveys (
     survey_id INT NOT NULL,
     studyspot_id INT NOT NULL,
     PRIMARY KEY (survey_id, studyspot_id), 
     survey_crowdednes_level INT NOT NULL, /*1, 2, 3, 4, 5 range*/
     survey_noise_level INT NOT NULL, /*1, 2, 3, 4, 5 range*/
+    survey_temp INT NOT NULL, /*1, 2, 3 range*/ 
     FOREIGN KEY (studyspot_id) REFERENCES Studyspots(studyspot_id)
 );
 
@@ -38,7 +51,6 @@ CREATE TABLE Reviews (
     review_comments VARCHAR(200),
     review_indoor BOOLEAN, /*indoor: TRUE, outdoor: FALSE*/
     review_wifi INT NOT NULL, /*1, 2, 3 range*/
-    review_temp INT NOT NULL, /*1, 2, 3 range*/
     review_rate FLOAT NOT NULL, /*1, 2, 3, 4, 5 range*/
     review_ada BOOLEAN, /*ADA accommodation: TRUE, NO ADA: FALSE*/
     review_power_outlets BOOLEAN,
