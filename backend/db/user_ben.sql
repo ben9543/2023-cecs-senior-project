c4562c9452a8e1be0e497c2808b1c0a9f5160a5a
Sunbeom Kweon (Ben) <bennie.ben095@gmail.com>
2023-09-13 18:22:01 -0700

    Update user_ben.sql

--- backend/db/user_ben.sql
+++ backend/db/user_ben.sql
@@ -1,40 +1,44 @@
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Universities (
+    university_id INT NOT NULL,
+    UNIQUE (university_id),
+    university_name VARCHAR(200),
+    university_state VARCHAR(2),
+    university_zip INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Studyspots (
+	  studyspot_id INT NOT NULL,
+	  studyspot_name VARCHAR(254),
+	  UNIQUE(studyspot_id)
+);
+
+CREATE TABLE IF NOT EXISTS Users (
     user_id INT NOT NULL, 
     user_email VARCHAR(254),
     user_name VARCHAR(200),
     password VARCHAR(512),
     university_id INT NOT NULL,
     user_photo VARCHAR(250),
-    PRIMARY KEY (user_id),
-    FOREIGN KEY (university_id) REFERENCES Universities(university_id)
-    --user_firstname VARCHAR(200)
-    --user_lastname VARCHAR(200)
-    --Recently Visited spots?
-
+    UNIQUE (user_id),
+	  CONSTRAINT FK_user_university FOREIGN KEY (university_id) REFERENCES Universities(university_id)
 );
 
-CREATE TABLE Universities (
-    university_id INT NOT NULL,
-    PRIMARY KEY (university_id),
-    university_name VARCHAR(200),
-    university_state VARCHAR(2),
-    university_zip INT NOT NULL
-);
-
-CREATE TABLE Surveys (
+CREATE TABLE IF NOT EXISTS Surveys (
     survey_id INT NOT NULL,
     studyspot_id INT NOT NULL,
-    PRIMARY KEY (survey_id, studyspot_id), 
-    survey_crowdednes_level INT NOT NULL, /*1, 2, 3, 4, 5 range*/
-    survey_noise_level INT NOT NULL, /*1, 2, 3, 4, 5 range*/
-    FOREIGN KEY (studyspot_id) REFERENCES Studyspots(studyspot_id)
+	  user_id INT NOT NULL,
+    UNIQUE (survey_id),
+    survey_crowdednes_level INT NOT NULL, 
+    survey_noise_level INT NOT NULL,
+	  CONSTRAINT FK_survey_studyspot FOREIGN KEY (studyspot_id) REFERENCES Studyspots(studyspot_id),
+    CONSTRAINT FK_survey_user FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Reviews (
+CREATE TABLE IF NOT EXISTS Reviews (
+	  review_id INT NOT NULL,
     user_id INT NOT NULL,
-    review_id INT NOT NULL,
     studyspot_id INT NOT NULL,
-    PRIMARY KEY (studyspot_id, user_id), 
+    Unique(review_id),
     review_comments VARCHAR(200),
     review_indoor BOOLEAN, /*indoor: TRUE, outdoor: FALSE*/
     review_wifi INT NOT NULL, /*1, 2, 3 range*/
@@ -43,27 +47,14 @@ CREATE TABLE Reviews (
     review_ada BOOLEAN, /*ADA accommodation: TRUE, NO ADA: FALSE*/
     review_power_outlets BOOLEAN,
     review_easy_to_find BOOLEAN,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (studyspot_id) REFERENCES Studyspots(studyspot_id)
+	  CONSTRAINT FK_review_studyspot FOREIGN KEY (studyspot_id) REFERENCES Studyspots(studyspot_id),
+    CONSTRAINT FK_review_user FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
-CREATE TABLE Favorites (
+CREATE TABLE IF NOT EXISTS Favorites (
     user_id INT NOT NULL,
     studyspot_id INT NOT NULL,
-    PRIMARY KEY (studyspot_id, user_id), 
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (studyspot_id) REFERENCES Studyspots(studyspot_id)
+    CONSTRAINT PK_fav PRIMARY KEY (user_id, studyspot_id), 
+	  CONSTRAINT FK_review_studyspot FOREIGN KEY (studyspot_id) REFERENCES Studyspots(studyspot_id),
+    CONSTRAINT FK_review_user FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
-
-
--- Features: list
--- Location: String
--- Indoor / outdoor: Boolean
--- Noise Level: From 1 to 5 (Float) => avg
--- Crowdedness Level: From 1 to 5 (Float) => avg
--- Strong WiFi: 1 to 3 (Float) => avg
--- Temperature: 1 to 3 (Float) => avg
--- Rate: Rating
--- Power outlets (optional)
--- ADA Accessible (optional)
--- Easy to find (optional)
