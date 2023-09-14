35440e492d2a0dec8152d8d866f7ca209fe29c99
ben9543 <sunbeom.kweon@gmail.com>
2023-09-13 19:27:53 -0700

    Added universities class

---
+++ backend/universities/users.py
@@ -0,0 +1,12 @@
+# from db.db_connection_test import Univ
+
+
+# API for CRUD Operations
+class Universities():
+    def __init__(self, db):
+        self.db = db
+
+    def get_studyspots(self, id):
+        # https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/quickstart/#installation
+        # self.db.session
+        pass
