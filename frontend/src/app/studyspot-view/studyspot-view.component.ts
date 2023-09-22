591836d64fac65fafb925daa937a301a0249c85f
the123tg30 <tony.guirguis@student.csulb.edu>
2023-09-21 17:43:35 -0700

    created virtual env and downloaded needed packages

--- frontend/src/app/studyspot-view/studyspot-view.component.ts
+++ frontend/src/app/studyspot-view/studyspot-view.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgFor} from '@angular/common';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @Component({
   selector: 'app-studyspot-view',
@@ -6,5 +12,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./studyspot-view.component.css']
 })
 export class StudyspotViewComponent {
+  myControl = new FormControl('');
+    // can list study spots to recommend here
+    options: string[] = [];
+  }
 
-}
