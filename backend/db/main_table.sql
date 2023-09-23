b2158f22525ffaf5c933dc523a1a08e44bc767e0
ben9543 <sunbeom.kweon@gmail.com>
2023-09-23 13:38:41 -0700

    Modified main_table.sql

--- backend/db/main_table.sql
+++ backend/db/main_table.sql
@@ -6,9 +6,13 @@ CREATE TABLE IF NOT EXISTS Universities (
 );
 
 CREATE TABLE IF NOT EXISTS Studyspots (
-	  studyspot_id INT NOT NULL,
-	  studyspot_name VARCHAR(254),
-	  UNIQUE(studyspot_id)
+    studyspot_id INT NOT NULL,
+    studyspot_name VARCHAR(254),
+    university_name VARCHAR(200),
+    -- indoor/outdoor 
+    -- image
+    UNIQUE(studyspot_name)
+    CONSTRAINT FK_studyspot_university FOREIGN KEY (university_name) REFERENCES Universities(university_name)
 );
 
 CREATE TABLE IF NOT EXISTS Users (
@@ -19,34 +23,33 @@ CREATE TABLE IF NOT EXISTS Users (
     university_name VARCHAR(200) NOT NULL,
     user_photo VARCHAR(250),
     UNIQUE (user_id),
-	  CONSTRAINT FK_user_university FOREIGN KEY (university_name) REFERENCES Universities(university_name)
+	CONSTRAINT FK_user_university FOREIGN KEY (university_name) REFERENCES Universities(university_name)
 );
 
 CREATE TABLE IF NOT EXISTS Surveys (
     survey_id INT NOT NULL,
     studyspot_id INT NOT NULL,
-	  user_id INT NOT NULL,
+	user_id INT NOT NULL,
     UNIQUE (survey_id),
     survey_crowdednes_level INT NOT NULL, 
     survey_noise_level INT NOT NULL,
-	  CONSTRAINT FK_survey_studyspot FOREIGN KEY (studyspot_id) REFERENCES Studyspots(studyspot_id),
+	CONSTRAINT FK_survey_studyspot FOREIGN KEY (studyspot_id) REFERENCES Studyspots(studyspot_id),
     CONSTRAINT FK_survey_user FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS Reviews (
-	  review_id INT NOT NULL,
+	review_id INT NOT NULL,
     user_id INT NOT NULL,
     studyspot_id INT NOT NULL,
     Unique(review_id),
     review_comments VARCHAR(200),
-    review_indoor BOOLEAN, /*indoor: TRUE, outdoor: FALSE*/
     review_wifi INT NOT NULL, /*1, 2, 3 range*/
     review_temp INT NOT NULL, /*1, 2, 3 range*/
     review_rate FLOAT NOT NULL, /*1, 2, 3, 4, 5 range*/
     review_ada BOOLEAN, /*ADA accommodation: TRUE, NO ADA: FALSE*/
     review_power_outlets BOOLEAN,
     review_easy_to_find BOOLEAN,
-	  CONSTRAINT FK_review_studyspot FOREIGN KEY (studyspot_id) REFERENCES Studyspots(studyspot_id),
+	CONSTRAINT FK_review_studyspot FOREIGN KEY (studyspot_id) REFERENCES Studyspots(studyspot_id),
     CONSTRAINT FK_review_user FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
