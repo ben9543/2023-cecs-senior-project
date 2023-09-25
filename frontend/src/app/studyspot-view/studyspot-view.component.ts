a549f3f701d376805af39413713955e3b8f37da1
Amanuel Reda <92605110+amanuelR@users.noreply.github.com>
2023-09-24 18:19:48 -0700

    studyspot view in-progress

--- frontend/src/app/studyspot-view/studyspot-view.component.ts
+++ frontend/src/app/studyspot-view/studyspot-view.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-studyspot-view',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class StudyspotViewComponent {
 
+  studySpot: any;
+  
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.studySpot = JSON.parse(params['data']);
+    });
+  }
+
+  checkIn() {
+    // Route to the Check-In survey component
+    this.router.navigate(['/check-in']);
+  }
+
+  rateMe() {
+    // Route to the Rate Me survey component
+    this.router.navigate(['/rate-me']);
+  }
 }
