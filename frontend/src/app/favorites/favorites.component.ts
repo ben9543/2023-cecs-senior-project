76c4fc72d0f7a7beb3aa3e171fbf9bb670106ad1
the123tg30 <tony.guirguis@student.csulb.edu>
2023-10-11 18:44:03 -0700

    Fixed number of spots in favorite

--- frontend/src/app/favorites/favorites.component.ts
+++ frontend/src/app/favorites/favorites.component.ts
@@ -10,15 +10,7 @@ export class FavoritesComponent {
     { name: "Japan", rating: "4", imageUrl: "assets/spots/Spot1.jpeg" },
     { name: "Uganda", rating: "3", imageUrl: "assets/spots/Spot1.jpeg" },
     { name: "Mexico", rating: "5", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "Canada", rating: "4", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "Denmark", rating: "3", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "China", rating: "5", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "Russia", rating: "4", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "France", rating: "3", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "Ireland", rating: "5", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "Brazil", rating: "4", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "England", rating: "3", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "Spain", rating: "5", imageUrl: "assets/spots/Spot1.jpeg" }
+    { name: "Canada", rating: "4", imageUrl: "assets/spots/Spot1.jpeg" }
   ];
   constructor() {}
 
