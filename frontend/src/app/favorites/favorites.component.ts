cf958466b711f4d9ae5e5a398ed3e7f78f02ac2c
the123tg30 <tony.guirguis@student.csulb.edu>
2023-10-18 13:09:52 -0700

    Pulled from dev branch/ updated favorites

--- frontend/src/app/favorites/favorites.component.ts
+++ frontend/src/app/favorites/favorites.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
-
+import { StudyspotService } from '../studyspot.service';
+import { AuthService } from '../auth.service';
+​
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.css']
 })
 export class FavoritesComponent {
-  spots: Spot = [
-    { name: "Japan", rating: "4", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "Uganda", rating: "3", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "Mexico", rating: "5", imageUrl: "assets/spots/Spot1.jpeg" },
-    { name: "Canada", rating: "4", imageUrl: "assets/spots/Spot1.jpeg" }
-  ];
-  constructor() {}
-
-  ngOnInit() {}
+  favspotnames: any;
+  userId: number = -1;
+  constructor(private studyspotService: StudyspotService, private authService: AuthService) { }
+  
+  ngOnInit(){
+    this.authService.userData$.subscribe((userData) => {
+      this.userId = userData.user_id;
+    });
+    this.loadStudySpotData();
+  }
+​
+  private loadStudySpotData() {
+    this.studyspotService.getAllLikedStudySpotsByUser(this.userId)
+      .subscribe((data: any) => {
+        this.favspotnames = data.data;
+      }, (error) => {
+        console.error("Error fetching study spot data:", error);
+      });
+  }
 }
-type Spot = Array<{ name: string; rating: string; imageUrl: string; }>;
