6ed0e9fe4d1824bce298835bbaa63d8b8c0aa4fb
Jose Fuentes <61329069+fuentesjm@users.noreply.github.com>
2023-10-21 23:53:21 -0700

    fixes reviews untill next sprint

--- frontend/src/app/reviews/reviews.component.ts
+++ frontend/src/app/reviews/reviews.component.ts
@@ -6,36 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./reviews.component.css']
 })
 export class ReviewsComponent {
-  spots: Spot = [
-    { name: "Horn Center", rating: "1", imageUrl: "assets/spots/Spot1.jpeg", userId: "John Smith", comment:"Horrible Place!"},
-    { name: "VEC Squad", rating: "2", imageUrl: "assets/spots/Spot1.jpeg", userId: "Karen White", comment:"giberish"}
-  ]
 
-  constructor() {}
-
-  ngOnInit() {}
-
-  selectedTabIndex: number = 0; // Initial selected tab index
-  tabs: number = this.spots.length; // Replace with your tab data
-
-  // Function to handle tab changes
-  tabChanged(event: number): void {
-    this.selectedTabIndex = event;
-  }
-
-  // Function to select the previous tab
-  selectPreviousTab(): void {
-    if (this.selectedTabIndex > 0) {
-      this.selectedTabIndex--;
-    }
-  }
-
-  // Function to select the next tab
-  selectNextTab(): void {
-    if (this.selectedTabIndex < this.tabs - 1) {
-      this.selectedTabIndex++;
-    }
-  }
 }
-
-type Spot = Array<{ name: string; rating: string; imageUrl: string; userId: string; comment: string; }>;
