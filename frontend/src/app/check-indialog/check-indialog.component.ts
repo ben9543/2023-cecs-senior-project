8531226e454443e41929fb487d4c054a82c5c965
salvador-villanueva <71253158+salvador-villanueva@users.noreply.github.com>
2023-10-22 18:13:39 -0700

    check-in dialog testing

--- frontend/src/app/check-indialog/check-indialog.component.ts
+++ frontend/src/app/check-indialog/check-indialog.component.ts
@@ -12,43 +12,23 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 })
 export class CheckIndialogComponent {
 
-  name: string = '';
-  userID: number = -1;
-  // for storing survey responses
-  crowdiness: number = 0;
-  wifi: number = 0;
-  noise: number = 0;
-
-  checkIn!:FormGroup;
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, 
-  private route: ActivatedRoute, private formBuilder: FormBuilder, 
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder, 
   private router: Router, private authService: AuthService, private studyspotService: StudyspotService) { }
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      this.name = params['name'];
-    });
-    this.checkIn = this.formBuilder.group({
-      crowdiness: ['', Validators.required],
-      wifi: ['', Validators.required],
-      noise: ['', Validators.required]
-    });
-    this.authService.userData$.subscribe((userData) => {
-      this.userID = userData.user_id;
-      console.log("userData in studyspot", userData);
-    });
   }
   min: number = 0;
   max: number = 5;
   step: number = 1;
-  finalValue: number = 0;
+  finalValueNoise: number = 0;
+  finalValueWifi: number = 0;
+  finalValueCrowdiness: number = 0;
+
+  checkIn(){
+    this.router.navigate(['/home']);
+  }
 
   onSubmit(){
-    if (this.checkIn.valid && this.userID != -1) {
-      const { comment } = this.checkIn.value;
-      console.log("Rate: ", this.finalValue)
-      console.log("UserId: ", this.userID)
-    }
   }
 
 }
