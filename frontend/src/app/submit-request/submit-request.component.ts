426b7c71a3cbea9aea87666fb04a787b4a8f0ca5
salvador-villanueva <71253158+salvador-villanueva@users.noreply.github.com>
2023-11-02 15:23:05 -0700

    basic text request form

--- frontend/src/app/submit-request/submit-request.component.ts
+++ frontend/src/app/submit-request/submit-request.component.ts
@@ -1,56 +1,6 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { UserService } from '../user.service';
-import { AuthService } from '../auth.service';
-import { ConfirmationDialogService } from '../confirmation-dialog.service';
-
-// validator to check if the username is empty
-const usernameEmptyValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-  const username = control.value;
-
-  if (!username || username.trim() === '') {
-    return { usernameEmpty: true };
-  }
-
-  return null;
-};
-
-// validator to check if the email is empty
-const emailEmptyValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-  const email = control.value;
-
-  if (!email || email.trim() === '') {
-    return { emailEmpty: true };
-  }
-
-  return null;
-};
-
-//validator to check if the email is valid
-const emailFormatValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-  const email = control.value;
-
-  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-  if (!emailPattern.test(email)) {
-    return { emailInvalid: true };
-  }
-
-  return null;
-};
-
-// validator to check if the college field is empty
-const collegeEmptyValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-  const college = control.value;
-
-  if (!college || college.trim() === '') {
-    return { collegeEmpty: true };
-  }
-
-  return null;
-};
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-submit-request',
@@ -58,58 +8,24 @@ const collegeEmptyValidator: ValidatorFn = (control: AbstractControl): Validatio
   styleUrls: ['./submit-request.component.css']
 })
 export class SubmitRequestComponent {
-  request!: FormGroup;
-  user: any = {}; // Initialize user object
-  userData: any | null = null;
-  universities: any;
+  request !: FormGroup;
 
-  constructor(private router: Router, private formBuilder: FormBuilder, private activeroute: ActivatedRoute,
-    private userService: UserService, private authService: AuthService, private snackBar: MatSnackBar, 
-    private confirmationDialogService: ConfirmationDialogService) { }
-  
-    ngOnInit(): void {
-      // Subscribe to userData$ and wait for it to emit data
-      this.authService.userData$.subscribe((userData) => {
-        this.userData = userData;
-    
-        // Once userData is available, initialize the form
-        this.initializeForm();
-      });
-    }
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
 
-    initializeForm(): void {
-      // Check if userData is available before initializing the form
-       this.userService.getUniversityList().subscribe((universities: any) => {
-          this.universities = universities.data;
-        });
-      if (this.userData) {
-        this.request = this.formBuilder.group({
-          username: [
-            this.userData.user_name,
-            [Validators.required, usernameEmptyValidator],
-          ],
-          email: [
-            this.userData.user_email,
-            [Validators.required, emailEmptyValidator, emailFormatValidator],
-          ],
-          college: [
-            this.userData.university_name,
-            [Validators.required, collegeEmptyValidator],
-          ],
-        });
-      }
+  ngOnInit(): void {
+    this.request = this.formBuilder.group({
+      university: ['', Validators.required],
+      location: ['', Validators.required],
+      description: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.request.valid) {
+      const { university, location, description } = this.request.value;
+      console.log(university);
+      console.log(location);
+      console.log(description);
     }
-    onSubmit(): void {
-      if (this.request.valid) {
-        const updatedUserData = {
-          user_id: this.userData.user_id,
-          // get these from the user's account
-          username: this.request.value.username,
-          university: this.request.value.college,
-          // request inputs
-          location: this.request.value.location,
-          comment: this.request.value.comment
-        };
-      }
-    }
+  }
 }
