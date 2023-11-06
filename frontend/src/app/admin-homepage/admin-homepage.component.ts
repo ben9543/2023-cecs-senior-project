2e565d460c5f4266600894682406e42be01ce019
the123tg30 <tony.guirguis@student.csulb.edu>
2023-11-06 10:40:18 -0800

    Admin Homepage complete??? Not sure

--- frontend/src/app/admin-homepage/admin-homepage.component.ts
+++ frontend/src/app/admin-homepage/admin-homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StudyspotService } from '../studyspot.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-admin-homepage',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-homepage.component.css']
 })
 export class AdminHomepageComponent {
-
+  reqspotnames: any;
+  userId: number = -1;
+  constructor(private studyspotService: StudyspotService, private authService: AuthService) { }
+  
+  ngOnInit(){
+    this.authService.userData$.subscribe((userData) => {
+      this.userId = userData.user_id;
+    });
+    this.loadStudySpotData();
+  }
+​
+  private loadStudySpotData() {
+    this.studyspotService.getAllLikedStudySpotsByUser(this.userId)
+      .subscribe((data: any) => {
+        this.reqspotnames = data.data;
+      }, (error) => {
+        console.error("Error fetching study spot data:", error);
+      });
+  }
 }
